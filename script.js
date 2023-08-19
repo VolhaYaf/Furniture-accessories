1c279d0825151308b69ff6ba372422a1553d857d
Volha <134659098+VolhaYaf@users.noreply.github.com>
2023-08-19 12:25:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const items = document.querySelectorAll('.item');
+items.forEach(item => {
+    item.addEventListener('mouseover', () =>{
+        removeFocus();
+        item.classList.add('expand');
+    })
+})
+
+removeFocus = () => {
+    items.forEach (item => {
+        item.classList.remove('expand');
+    })
+}
+
+function openingCountDown() {
+    const openingDate = new Date('September 10, 2023 12:00');
+    const now = new Date();
+    const difference = openingDate - now;
+
+
+    const msInSeconds = 1000; //сколько миллисекунд в секунде
+    const msInMinutes = 60 * 1000; //сколько миллисекунд в минуте
+    const msInHours = 60 * 60 * 1000; //сколько миллисекунд в часе
+    const msInDays = 24 * 60 * 60 * 1000; //сколько миллисекунд в сутках
+
+    const displayDay = Math.floor(difference / msInDays);
+    document.querySelector('.days').textContent = displayDay;
+
+    const displayHour = Math.floor(difference % msInDays / msInHours);
+    document.querySelector('.hours').textContent = displayHour;
+
+    const displayMinute = Math.floor(difference % msInHours / msInMinutes);
+    document.querySelector('.minutes').textContent = displayMinute;
+
+    const displaySecond = Math.floor(difference % msInMinutes / msInSeconds);
+    document.querySelector('.seconds').textContent = displaySecond;
+
+    if (difference <= 0) {   //чтобы время не уходило в минус
+        document.querySelector('.days').textContent = 0;
+        document.querySelector('.hours').textContent = 0;
+        document.querySelector('.minutes').textContent = 0;
+        document.querySelector('.seconds').textContent = 0;
+        clearInterval(timerID); //чтобы остановить счетчик. Этот метод всегда идет в связке с setInterval 
+        weAreOpen(); 
+}
+}
+
+function weAreOpen() {
+    const heading = document.querySelector('h1');
+    heading.textContent = 'We are finally open!!!';
+    heading.classList.add('change');
+}
+
+let timerID = setInterval(openingCountDown, 1000); 
